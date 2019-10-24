47f7b66704c70efe08f7195b23a31d7b6833e900
MobixMAC04 <mobixmac04@MobixMAC04s-iMac.local>
2019-10-24 09:42:47 +0700

    updated response pdf file to user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { createReadStream } from 'fs';
+import { readFileSync } from 'fs';
 import { Controller, Get, HttpCode, Header, HttpStatus, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
@@ -15,17 +15,17 @@ export class AppController {
 
   @Get('pdf')
   @HttpCode(HttpStatus.OK)
-  @Header('Content-Type', 'application/pdf')
-  @Header('Content-Disposition', 'attachment; filename=invoice-test.pdf')
-  async pdf(@Res() res: Response) {
+  pdf(@Res() res: Response) {
     res.set({
       'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=invoice-test.pdf'
     });
     this.appService.createHtml();
-    const pdfStream = this.appService.createPdf();
+    // const pdfStream = this.appService.createPdf();
 
     console.log('joined', joinedPdfFilePath);
+    const pdfFile = readFileSync(joinedPdfFilePath);
 
-    pdfStream.pipe(res);
+    res.send(pdfFile);
   }
 }
